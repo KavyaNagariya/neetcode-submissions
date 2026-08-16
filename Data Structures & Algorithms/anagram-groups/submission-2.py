085a390cae754bdf27a44086beb71dc750248692
KavyaNagariya <mehukavyanagariya@gmail.com>
2026-08-16 12:02:34 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for word in strs:
+            sortedS = ''.join(sorted(word))
+            result[sortedS].append(word)
+        return list(result.values())    
