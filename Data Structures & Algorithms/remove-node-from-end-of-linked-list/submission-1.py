7c411a029a2121bba1269505d91b31d15c70eb28
KavyaNagariya <mehukavyanagariya@gmail.com>
2026-09-08 23:38:22 +0530

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # 1st approach - reverse the list , remove the nth node , reverse the list again 
+        '''
+        prev, curr = None, head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        newHead = prev
+        curr = newHead
+        prev = None
+        while n > 0:
+            n -= 1
+            prev = curr
+            curr = curr.next
+        
+        if n == 0:
+            prev = curr.next
+            curr.next = None
+        
+        againPrev, againCurr = None, newHead
+        while againCurr:
+            next = againCurr.next
+            againCurr.next = againPrev
+            againPrev = againCurr
+            againCurr = next
+        return againPrev
+        '''
+        # optimized solution moving the n nodes ahead till reached the end(tail.next = None) then just remove the left node (used dummy node and just changed the left node's previous node's next to left.next)
+
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+        
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+        return dummy.next 
+
